9c50f00ec582ff7eb6f049c3107445ff050a54ff
Henry Smith <henry@henrysmith.org>
2011-09-03 11:33:01 +0100

    adds HttpResponseTest

---
+++ tests/HttpResponseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once '../src/HttpResponse.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+
+use \PHPUnit_Framework_TestCase;
+use \RedditApiClient\HttpResponse;
+
+/**
+ * HttpResponseTest 
+ *
+ * Tests HttpResponse's data encapsulation
+ * 
+ * @author    Henry Smith <henry@henrysmith.org> 
+ * @copyright 2011 Henry Smith
+ * @license   GPLv2.0
+ * @package   Reddit API Client
+ * @version   0.00
+ */
+class HttpResponseTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Verifies that the slightly unpleasant-looking string matching in
+	 * getHeader() actually works
+	 */
+	public function testGetHeaderFindsCorrectHeader()
+	{
+		$headers = array(
+			"X-Test-Header: 1122334455\r\n",
+			"X-Test-Header-2: 99887744\r\n",
+		);
+
+		$response = new HttpResponse($headers, '');
+
+		$this->assertEquals('1122334455', $response->getHeader('X-Test-Header'));
+		$this->assertEquals('99887744', $response->getHeader('X-Test-Header-2'));
+		$this->assertEquals(null, $response->getHeader('Content-Type'));
+	}
+
+}
+

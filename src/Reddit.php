eb466a6d327d5ae68d9fe311cdd785ca6559210f
Henry Smith <henry@henrysmith.org>
2011-09-03 11:33:39 +0100

    documentation

--- src/Reddit.php
+++ src/Reddit.php
@@ -2,8 +2,29 @@
 
 namespace RedditApiClient;
 
+/**
+ * Reddit 
+ *
+ * The main class of the API client, handling its state and the sending of
+ * queries to the API 
+ * 
+ * @author    Henry Smith <henry@henrysmith.org> 
+ * @copyright 2011 Henry Smith
+ * @license   GPLv2.0
+ * @package   Reddit API Client
+ * @version   0.00
+ */
 class Reddit {
 
+	/**
+	 * Creates the API client instance
+	 *
+	 * If given a username and password, will attempt to login.
+	 * 
+	 * @access public
+	 * @param  string $username [optional]  The username to login with
+	 * @param  string $password [optional]  The password to login with
+	 */
 	public function __construct($username, $password)
 	{
 		if ($username && $password && !$this->login()) {

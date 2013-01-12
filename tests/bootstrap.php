f79bbe51c75e909baf6960806da1f0796284dc37
Henry Smith <henry@henrysmith.org>
2013-01-12 11:44:04 +0000

    Rearrange the directory structure in a desperate attepmt to get composer working

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../src/RedditApiClient/Reddit.php';
+require_once __DIR__ . '/../src/RedditApiClient/Entity.php';
+require_once __DIR__ . '/../src/RedditApiClient/Account.php';
+require_once __DIR__ . '/../src/RedditApiClient/Comment.php';
+require_once __DIR__ . '/../src/RedditApiClient/Link.php';
+require_once __DIR__ . '/../src/RedditApiClient/HttpRequest.php';
+require_once __DIR__ . '/../src/RedditApiClient/HttpResponse.php';
+require_once __DIR__ . '/../src/RedditApiClient/RedditException.php';
+require_once __DIR__ . '/../src/RedditApiClient/Subreddit.php';
+
+require_once __DIR__ . '/Fake/Entity.php';
+require_once __DIR__ . '/Stub/RedditLoginRefuser.php';
+require_once __DIR__ . '/Stub/RedditNoRequests.php';
+

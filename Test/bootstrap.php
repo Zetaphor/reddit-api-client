3a4c15ff024bd24fa24d4797d2a5bc4d60964d16
Henry Smith <henry@henrysmith.org>
2013-01-12 11:09:34 +0000

    Get the test suite working again in PSR2 compliance

---
+++ Test/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../Reddit.php';
+require_once __DIR__ . '/../Entity.php';
+require_once __DIR__ . '/../Account.php';
+require_once __DIR__ . '/../Comment.php';
+require_once __DIR__ . '/../Link.php';
+require_once __DIR__ . '/../HttpRequest.php';
+require_once __DIR__ . '/../HttpResponse.php';
+require_once __DIR__ . '/../RedditException.php';
+require_once __DIR__ . '/../Subreddit.php';
+
+require_once __DIR__ . '/Fake/Entity.php';
+require_once __DIR__ . '/Stub/RedditLoginRefuser.php';
+require_once __DIR__ . '/Stub/RedditNoRequests.php';
+

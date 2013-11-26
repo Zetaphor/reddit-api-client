785bd06350e80cac7cd565047628180de80e814d
Henry Smith <henry@henrysmith.org>
2013-11-26 19:40:35 +0000

    Remove legacy client code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,3 @@
 <?php
-
-require_once __DIR__ . '/../src/RedditApiClient/Reddit.php';
-require_once __DIR__ . '/../src/RedditApiClient/Entity.php';
-require_once __DIR__ . '/../src/RedditApiClient/Account.php';
-require_once __DIR__ . '/../src/RedditApiClient/Comment.php';
-require_once __DIR__ . '/../src/RedditApiClient/Link.php';
-require_once __DIR__ . '/../src/RedditApiClient/HttpRequest.php';
-require_once __DIR__ . '/../src/RedditApiClient/HttpResponse.php';
-require_once __DIR__ . '/../src/RedditApiClient/RedditException.php';
-require_once __DIR__ . '/../src/RedditApiClient/Subreddit.php';
-
-require_once __DIR__ . '/Fake/Entity.php';
-require_once __DIR__ . '/Stub/RedditLoginRefuser.php';
-require_once __DIR__ . '/Stub/RedditNoRequests.php';
-
 require_once __DIR__ . '/../vendor/autoload.php';
 

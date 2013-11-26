a394660da8268f5834c3b1586a2989410964dec3
Henry Smith <henry@henrysmith.org>
2013-11-26 07:57:26 +0000

    Create session chain storage

---
+++ src/RedditApiClient/Session/Storage/Chain.php
@@ -0,0 +1,32 @@
+<?php
+namespace RedditApiClient\Session\Storage;
+
+use RedditApiClient\Session;
+
+class Chain implements Session\Storage
+{
+	private $storageImplementations = array();
+
+	public function __construct(array $storageImplementations = array())
+	{
+		$this->storageImplementations = $storageImplementations;
+	}
+
+	public function storeSession(Session $session)
+	{
+		foreach ($this->storageImplementations as $storage) {
+			$storage->storeSession($session);
+		}
+	}
+
+	public function retrieveSession($username)
+	{
+		foreach ($this->storageImplementations as $storage) {
+			$session = $storage->retrieveSession($username);
+			if ($session instanceof Session) {
+				return $session;
+			}
+		}
+	}
+}
+
---
+++ tests/Session/Storage/ChainTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace RedditApiClient\Test\Session\Storage;
+
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+use RedditApiClient\Session;
+
+class ChainTest extends PHPUnit_Framework_TestCase
+{
+	private $chain;
+	private $session;
+	private $firstImplementation;
+	private $secondImplementation;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->session = new Session('exampleuser', 'swordfish');
+		$this->firstImplementation = m::mock('RedditApiClient\Session\Storage');
+		$this->secondImplementation = m::mock('RedditApiClient\Session\Storage');
+		$this->chain = new Session\Storage\Chain(
+			array(
+				$this->firstImplementation,
+				$this->secondImplementation
+			)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function storeSession()
+	{
+		$this->firstImplementation
+			->shouldReceive('storeSession')
+			->with($this->session)
+			->once();
+
+		$this->secondImplementation
+			->shouldReceive('storeSession')
+			->with($this->session)
+			->once();
+
+		$this->chain->storeSession($this->session);
+	}
+
+	/**
+	 * @test
+	 */
+	public function retrieveSessionStopsWhenFound()
+	{
+		$this->firstImplementation
+			->shouldReceive('retrieveSession')
+			->with('example')
+			->andReturn($this->session)
+			->once();
+
+		$session = $this->chain->retrieveSession('example');
+		$this->assertEquals($this->session, $session);
+	}
+}
+

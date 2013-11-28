130cf7b0315ed0110abc128c3f6661905c951704
Henry Smith <henry@henrysmith.org>
2013-11-28 20:36:04 +0000

    Thing factory operational

--- src/Reddit/Thing/Factory.php
+++ src/Reddit/Thing/Factory.php
@@ -5,9 +5,21 @@ use Reddit\Thing;
 
 class Factory
 {
+	private $kindClassMap = array(
+		't1' => 'Reddit\Thing\Comment',
+		't2' => 'Reddit\Thing\Account',
+		't3' => 'Reddit\Thing\Link',
+		't4' => 'Reddit\Thing\Message',
+		't5' => 'Reddit\Thing\Subreddit',
+		't6' => 'Reddit\Thing\Award',
+		't7' => 'Reddit\Thing\PromoCampaign',
+	);
+
 	public function createThing(array $input)
 	{
-		$thing = new Thing\Account;
+		if (isset($this->kindClassMap[$input['kind']])) {
+			$thing = new $this->kindClassMap[$input['kind']];
+		}
 		return $thing;
 	}
 }
--- tests/Thing/FactoryTest.php
+++ tests/Thing/FactoryTest.php
@@ -16,19 +16,91 @@ class FactoryTest extends PHPUnit_Framework_TestCase
 	/**
 	 * @test
 	 */
+	public function createComment()
+	{
+		$input = array(
+			'kind' => 't1',
+			'data' => array(),
+		);
+		$thing = $this->factory->createThing($input);
+		$this->assertTrue($thing instanceof Thing\Comment);
+	}
+
+	/**
+	 * @test
+	 */
 	public function createAccount()
 	{
 		$input = array(
 			'kind' => 't2',
-			'data' => array(
-				'created'     => '1377035773.0',
-				'created_utc' => '1377035773.0',
-				'has_mail'    => true,
-				'is_friend'   => true,
-				'name'        => 'jedberg',
-			),
+			'data' => array(),
 		);
 		$thing = $this->factory->createThing($input);
 		$this->assertTrue($thing instanceof Thing\Account);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createLink()
+	{
+		$input = array(
+			'kind' => 't3',
+			'data' => array(),
+		);
+		$thing = $this->factory->createThing($input);
+		$this->assertTrue($thing instanceof Thing\Link);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createMessage()
+	{
+		$input = array(
+			'kind' => 't4',
+			'data' => array(),
+		);
+		$thing = $this->factory->createThing($input);
+		$this->assertTrue($thing instanceof Thing\Message);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createSubreddit()
+	{
+		$input = array(
+			'kind' => 't5',
+			'data' => array(),
+		);
+		$thing = $this->factory->createThing($input);
+		$this->assertTrue($thing instanceof Thing\Subreddit);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createAward()
+	{
+		$input = array(
+			'kind' => 't6',
+			'data' => array(),
+		);
+		$thing = $this->factory->createThing($input);
+		$this->assertTrue($thing instanceof Thing\Award);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createPromoCampaign()
+	{
+		$input = array(
+			'kind' => 't7',
+			'data' => array(),
+		);
+		$thing = $this->factory->createThing($input);
+		$this->assertTrue($thing instanceof Thing\PromoCampaign);
 	}
 }
